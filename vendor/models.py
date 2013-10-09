b73c78ccbd16c30b9e5bcd38ded7304497b2c52c
Joshua Michael <dottru@gmail.com>
2013-10-09 04:03:18 +0000

    save vendor applications

--- vendor/models.py
+++ vendor/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 from django.forms import ModelForm
 from django.contrib.auth.models import User 
+from django.contrib import admin
 
 class Vendor(models.Model):
   user = models.ForeignKey(User, unique=True)
+  active = models.BooleanField(default=False)
+
   vendorname = models.CharField(max_length=64)
   profile = models.TextField()
-  active = models.BooleanField(default=False)
   pubkey = models.TextField()
 
   @classmethod
   def is_vendor(cls, user):
     return (cls.objects.filter(user=user,active=True).count() > 0)
+  def __unicode__(self): return self.vendorname
 
 class VendorForm(ModelForm):
   class Meta:
     model = Vendor
-    fields = ['vendorname', 'profile']
+    fields = ['vendorname', 'profile', 'pubkey']
 
+admin.site.register(Vendor)
--- vendor/views.py
+++ vendor/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from listing.models import *
+from vendor.models import VendorForm, Vendor
+from django.http import HttpResponseRedirect
 
 def index(req):
   return render(req, 'vendor/index.html', {})
 
 def register(req):
-  return render(req, 'vendor/register.html', {})
+  if req.method == 'POST':
+    form = VendorForm(req.POST)
+    if form.is_valid():
+      new = Vendor(
+          user       = req.user,
+          vendorname = form.cleaned_data['vendorname'],
+          profile    = form.cleaned_data['profile'],
+          pubkey     = form.cleaned_data['pubkey']
+      )
+      new.save()
+
+      return HttpResponseRedirect('/account/thanks')
+  else:
+    form = VendorForm()
+
+  return render(req, 'vendor/register.html', {'form':form})
 
 def search(req):
   return render(req, 'vendor/search.html', {})
